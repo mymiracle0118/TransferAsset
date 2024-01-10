326685059d07f42613143c9b0aac673db9510aaa
mymiracle0118 <noahsflood908@gmail.com>
2024-01-10 19:19:36 +0900

    Modify code

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 export default function App(){
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const endpoint = useMemo();
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const wallets = useMemo(() => [getPhantomWallet(),getSolletExtensionWallet(),getSolflareWallet(),getLedgerWallet()], []);
   return (
     <ConnectionProvider endpoint={endpoint}>
