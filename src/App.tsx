3c2a04a777cbf490dec6710aebc24963708a32e3
mymiracle0118 <noahsflood908@gmail.com>
2024-01-10 20:47:37 +0900

    Modify data > > Co-authored-by: mymiralce0118 noahsflood908@gmail.com Co-authored-by: dreamDev525 dreamdev525@gmail.com

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 export default function App(){
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const network = WalletAdapterNetwork.Devnet;
+  // const network = WalletAdapterNetwork.Devnet;
   // const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   // const wallets = useMemo(() => [getPhantomWallet(),getSolletExtensionWallet(),getSolflareWallet(),getLedgerWallet()], []);
   return (
